d6df71d204ca76bf8590d273f129a2ec3ffcfcbd
Amit_Kaspi <akaspi@wix.com>
2015-07-30 17:25:24 +0300

    Add requirejs support

--- public/main.js
+++ public/main.js
@@ -1,14 +1,12 @@
 requirejs.config({
     baseUrl: "src",
     paths: {
-      "react": "https://fb.me/react-0.13.3.min",
-      "JSXTransformer": "https://fb.me/JSXTransformer-0.13.3"
+      react: "https://fb.me/react-0.13.3",
+      JSXTransformer: "https://fb.me/JSXTransformer-0.13.3"
     }
 });
 
 requirejs(['react', 'components/helloWorld'], function(React, HelloWorld) {
-  React.render(
-    <HelloWorld />,
-    document.getElementById('view')
-  );
+  var element = React.createElement(HelloWorld);
+  React.render(element, document.getElementById('view'));
 });
