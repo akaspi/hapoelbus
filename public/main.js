349fc00b1560f4a51b7bc81f620657aea2b2952a
Amit_Kaspi <akaspi@wix.com>
2015-07-30 19:02:05 +0300

    Load monified version of lodash

--- public/main.js
+++ public/main.js
@@ -2,7 +2,7 @@ requirejs.config({
     baseUrl: "src",
     paths: {
       react: "https://fb.me/react-0.13.3",
-      lodash: "https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.10.0/lodash"
+      lodash: "https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.10.0/lodash.min"
     },
     shim: {
       react: { exports: 'React' },
