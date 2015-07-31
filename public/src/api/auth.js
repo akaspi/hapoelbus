41c5369cda6884a142ac0d7c7e519ce40063eeb8
Amit_Kaspi <akaspi@wix.com>
2015-07-31 17:06:16 +0300

    Basic implementation for auth module

--- public/src/api/auth.js
+++ public/src/api/auth.js
@@ -1,11 +1,38 @@
 define(['lodash', 'api/store'], function(_, store) {
 
+  var paths = {
+    admins: 'admins'
+  }
+
+  function onAuthActionComplete(onSuccess, onError, errorData, authData) {
+      if (errorData) {
+        onError();
+      } else {
+        onSuccess();
+      }
+  }
+
   return {
-    createUser: _.noop,
-    login: _.noop,
-    // loginWithFacebook: _.noop,
-    // loginWithGoogle: _.noop,
-    logut: _.noop,
-    isAdmin: _.noop,
+    createUser: function(email, password, onSuccess, onError) {
+      store.createUser({
+        email: email,
+        password: password
+      }, onAuthActionComplete.bind(null, onSuccess, onError));
+    },
+    login: function(email, password, onSuccess, onError) {
+      store.authWithPassword({
+        email: email,
+        password: password
+      }, onAuthActionComplete.bind(null, onSuccess, onError));
+    },
+    loginWithFacebook: function(onSuccess, onError) {
+      store.authWithOAuthPopup('facebook', onAuthActionComplete.bind(null, onSuccess, onError), { scope: 'email' });
+    },
+    isLoggedIn: function() {
+      return !!store.getAuth();
+    },
+    logut: function() {
+      store.unauth();
+    }
   };
 });
