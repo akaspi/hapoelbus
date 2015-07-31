b2ff8102f6c67cffbc13cfde36b751d7324647f5
Amit_Kaspi <akaspi@wix.com>
2015-07-31 16:10:34 +0300

    Add Firebase to requirejs dependencies

--- public/main.js
+++ public/main.js
@@ -2,15 +2,17 @@ requirejs.config({
     baseUrl: "src",
     paths: {
       react: "https://fb.me/react-0.13.3",
-      lodash: "https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.10.0/lodash.min"
+      lodash: "https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.10.0/lodash.min",
+      firebase: "https://cdn.firebase.com/js/client/2.2.8/firebase"
     },
     shim: {
       react: { exports: 'React' },
-      lodash: { exports: '_' }
+      lodash: { exports: '_' },
+      firebase: { exports: 'Firebase' }
     }
 });
 
-requirejs(['react', 'components/helloWorld', 'lodash'], function(React, HelloWorld, _) {
+requirejs(['react', 'components/helloWorld'], function(React, HelloWorld) {
   var element = React.createElement(HelloWorld);
   React.render(element, document.getElementById('view'));
 });
