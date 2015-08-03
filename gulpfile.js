697cafd0acd146ffb98d81f83d8e6a4b1c577af0
Amit_Kaspi <akaspi@wix.com>
2015-08-03 15:47:18 +0300

    Add gulp dev watch task

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,9 @@
 var gulp = require('gulp');
-
-var bases = {
- app: 'app/',
- dist: 'dist/',
-};
-
-var paths = {
- scripts: ['scripts/**/*.js', '!scripts/libs/**/*.js'],
- libs: ['scripts/libs/jquery/dist/jquery.js', 'scripts/libs/underscore/underscore.js', 'scripts/backbone/backbone.js'],
- styles: ['styles/**/*.css'],
- html: ['index.html', '404.html'],
- images: ['images/**/*.png'],
- extras: ['crossdomain.xml', 'humans.txt', 'manifest.appcache', 'robots.txt', 'favicon.ico'],
-};
-
+var watch = require('gulp-watch');
+var sass = require('gulp-sass');
+var rt = require('gulp-react-templates');
 
 gulp.task('sass', function () {
-  var sass = require('gulp-sass');
   gulp.src('./public/app.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./public/'));
@@ -26,27 +13,16 @@ gulp.task('sass:watch', function () {
   gulp.watch('./**/*.scss', ['sass']);
 });
 
-var rt = require('gulp-react-templates');
-
 gulp.task('rt', function() {
     gulp.src('./public/src/**/*.rt')
         .pipe(rt({modules: 'amd'}))
+        .on('error', function(error) { console.log(error.message) })
         .pipe(gulp.dest('./public/src'));
 });
 
 gulp.task('dev', function() {
   gulp.watch('./**/*.scss', ['sass']);
-  gulp.watch('./**/*.rt', ['rt']);
+  watch('./**/*.rt', function() { gulp.run('rt'); });
 });
 
-// var react = require('gulp-react');
-// gulp.task('transformJSX', function (x) {
-//     return gulp.src('**/*.jsx', { cwd: './public/src' })
-//         .pipe(react())
-//         .pipe(gulp.dest('./public/src'));
-// });
-// gulp.task('watchJSX', function() {
-//   gulp.watch('./public/src/**/*.jsx', ['transformJSX']);
-// });
-
-gulp.task('default', ['sass']);
+gulp.task('default', ['rt', 'sass']);
