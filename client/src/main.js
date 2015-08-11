d98133d66acc71fa035fcb6a712ed1c0bd363d5d
Amit_Kaspi <akaspi@wix.com>
2015-08-11 13:04:54 +0300

    Add injectTapEventPlugi

--- client/src/main.js
+++ client/src/main.js
@@ -2,6 +2,8 @@
     var React = require('react/addons');
     var RootComponent = require('./components/root/root');
 
+    require("react-tap-event-plugin")();
+
     window.React = React;
 
     React.render(React.createElement(RootComponent), document.body);
