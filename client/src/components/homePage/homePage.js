98724cba6433de99d4b4148c0d997249ef9acdf7
chene <chene@wix.com>
2015-08-12 21:13:49 +0300

    added home page

---
+++ client/src/components/homePage/homePage.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var React = require('react/addons');
+var template = require('./homePage.rt.js');
+
+var Root = React.createClass({
+    render: template
+});
+
+module.exports = Root;
