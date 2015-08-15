4bba8f5441743055c66e29e0d07616f0034b2369
chene <chene@wix.com>
2015-08-15 20:15:15 +0300

    added premium checkbox to the account data

--- client/src/components/accountData/accountData.js
+++ client/src/components/accountData/accountData.js
@@ -7,18 +7,22 @@ var template = require('./accountData.rt.js');
 var userActions = require('../../actions/userActions');
 
 var AccountData = React.createClass({
-    mixins: [ React.addons.LinkedStateMixin, muiMixin ],
+    mixins: [React.addons.LinkedStateMixin, muiMixin],
     getInitialState: function () {
         return {
             email: '',
             displayName: '',
             phone: '',
-            maxSeats: ''
+            maxSeats: '',
+            isPremium: false
         }
     },
     handleCreateAccount: function () {
         var userData = this.state;
         userActions.createUserData(userData);
+    },
+    handlePremiumChange: function (e, value) {
+        this.setState({isPremium: value});
     },
     render: template
 });
