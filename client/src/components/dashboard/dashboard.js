dde07017b528e4447586fdbf11817ba2311aa187
Amit_Kaspi <akaspi@wix.com>
2015-08-22 19:39:41 +0300

    many css changes

--- client/src/components/dashboard/dashboard.js
+++ client/src/components/dashboard/dashboard.js
@@ -4,23 +4,9 @@ var React = require('react/addons');
 var muiMixin = require('../mixins/mui-mixin');
 
 var template = require('./dashboard.rt.js');
-var userActions = require('../../actions/userActions');
-
-var tabOptions = ['חדשות ועדכונים', 'פרטים אישיים'];
 
 var AccountData = React.createClass({
     mixins: [React.addons.LinkedStateMixin, muiMixin],
-    getInitialState: function () {
-        return {
-            selectedTabIndex: 0
-        }
-    },
-    getTabOptions: function () {
-        return tabOptions;
-    },
-    handleTabChange: function (tabIndex){
-      this.setState({selectedTabIndex: tabIndex});
-    },
     render: template
 });
 
