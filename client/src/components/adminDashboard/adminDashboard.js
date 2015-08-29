c03fbc5a2d95ffd0fdb27eb4be77a42f65ceb8bf
Amit_Kaspi <akaspi@wix.com>
2015-08-29 23:04:16 +0300

    display filter drop down on usersData

--- client/src/components/adminDashboard/adminDashboard.js
+++ client/src/components/adminDashboard/adminDashboard.js
@@ -6,10 +6,18 @@ var muiMixin = require('../mixins/mui-mixin');
 var adminStore = require('../../stores/adminStore');
 var adminActions = require('../../actions/adminActions');
 
+var USERS_DATA_FILTER_OPTIONS = {
+    ALL: 'all',
+    PAID: 'paid',
+    FREE: 'free'
+};
+
 var AdminDashboard = React.createClass({
     mixins: [ muiMixin ],
     getInitialState: function () {
-        return adminStore.getAll();
+        return _.merge({
+            usersDataFilter: USERS_DATA_FILTER_OPTIONS.ALL
+        }, adminStore.getAll());
     },
     componentDidMount: function () {
         adminStore.registerToChange(this.onAdminStoreDataChanged);
@@ -21,6 +29,30 @@ var AdminDashboard = React.createClass({
     },
     componentWillUnmount: function() {
         adminStore.removeChangeListener(this.onAdminStoreDataChanged);
+    },
+    getUsersDataFilterItems: function() {
+      return [
+          { payload: USERS_DATA_FILTER_OPTIONS.ALL, text: 'כל המשתמשים' },
+          { payload: USERS_DATA_FILTER_OPTIONS.PAID, text: 'מנויים בלבד' },
+          { payload: USERS_DATA_FILTER_OPTIONS.FREE, text: 'חד פעמיים' }
+      ];
+    },
+    filterUsersData: function(e, index, filterItem) {
+        this.setState({usersDataFilter: filterItem.payload});
+    },
+    getFilteredUsersData: function() {
+        switch (this.state.usersDataFilter) {
+            case USERS_DATA_FILTER_OPTIONS.ALL:
+                return this.state.usersData;
+            case USERS_DATA_FILTER_OPTIONS.PAID:
+                return _.pick(this.state.usersData, function(userData, uid) {
+                    return _.contains(_.keys(this.state.payments), uid)
+                }, this);
+            case USERS_DATA_FILTER_OPTIONS.FREE:
+                return _.pick(this.state.usersData, function(userData, uid) {
+                    return !_.contains(_.keys(this.state.payments), uid)
+                }, this);
+        }
     },
     render: template
 });
