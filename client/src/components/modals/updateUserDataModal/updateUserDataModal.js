1d7a5f12a00bb851c842c6ad8b321c24edd76e24
Amit_Kaspi <akaspi@wix.com>
2015-09-04 12:54:33 +0300

    updateUserData modal css

--- client/src/components/modals/updateUserDataModal/updateUserDataModal.js
+++ client/src/components/modals/updateUserDataModal/updateUserDataModal.js
@@ -3,19 +3,23 @@
 var React = require('react/addons');
 var template = require('./updateUserDataModal.rt.js');
 var muiMixin = require('../../mixins/mui-mixin');
+var _ = require('lodash');
 
 var modalActions = require('../../../actions/modalActions');
 
 var ModalLayer =  React.createClass({
     mixins: [ muiMixin, React.addons.LinkedStateMixin ],
     getInitialState: function() {
-        return this.props.userData;
+        return this.props.userData[_.keys(this.props.userData)[0]];
     },
     componentDidMount: function() {
         this.refs.modal.show();
     },
     onDialogClose: function() {
         modalActions.hideCurrentModal();
+    },
+    toggleIsPremium: function(e, val) {
+        this.setState({isPremium: val});
     },
     render: template
 });
