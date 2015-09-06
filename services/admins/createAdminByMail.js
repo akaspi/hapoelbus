e40fa82da6b019eab6a47ea625167a3c3d96461b
Amit_Kaspi <akaspi@wix.com>
2015-09-06 19:09:44 +0300

    fix createAdminByMail flow

--- services/admins/createAdminByMail.js
+++ services/admins/createAdminByMail.js
@@ -4,15 +4,16 @@ var utils = require('../utils.js');
 var _ = require('lodash');
 var ref = utils.getFirebaseRef();
 
-var args = process.argv.splice(2);
-
-if (_.isEmpty(args)) {
-    console.log('Service should get an email as an argument');
-    process.exit();
-}
-
 utils.loginAsAdmin(ref, function () {
+    var args = process.argv.splice(2);
+
+    if (_.isEmpty(args)) {
+        console.log('Service should get an email as an argument');
+        process.exit();
+    }
+
     var adminUid = args[0];
+    
     ref.child('admins').child(adminUid).set(true, function (error) {
         if (error) {
             console.log('Failed to add admin');
