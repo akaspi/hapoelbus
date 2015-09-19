b399d69f2e9b2b660ac9bee86bf7f0ef0c6f8e28
Amit_Kaspi <akaspi@wix.com>
2015-09-19 21:23:24 +0300

    add filters to usersData explorer

--- client/src/components/dataExplorer/usersDataExplorer/usersDataExplorer.js
+++ client/src/components/dataExplorer/usersDataExplorer/usersDataExplorer.js
@@ -13,12 +13,34 @@ var paymentsActions = require('../../../actions/paymentsActions');
 
 var dataSchemas = require('../../../../../common/dataSchemas');
 
+var FILTERS = {
+    ALL_USERS: 'ALL_USERS',
+    MEMBERS_ONLY: 'MEMBERS_ONLY',
+    FREE_ONLY: 'FREE_ONLY',
+    ASKED_FOR_MEMBERSHIP: 'ASKED_FOR_MEMBERSHIP'
+};
+
+function filterUsersData(filter, usersData) {
+    switch (filter) {
+        case FILTERS.ALL_USERS:
+            return usersData;
+        case FILTERS.MEMBERS_ONLY:
+            return _.omit(usersData, function(userData) { return userData.maxSeats === 0 });
+        case FILTERS.FREE_ONLY:
+            return _.omit(usersData, function(userData) { return userData.maxSeats > 0 });
+        case FILTERS.ASKED_FOR_MEMBERSHIP:
+            return _.omit(usersData, function(userData) { return !userData.isPremium });
+    }
+}
+
 var GamesExplorer = React.createClass({
+    mixins: [React.addons.LinkedStateMixin],
     getInitialState: function () {
         var paymentsState = paymentsStore.getAll();
         return {
             isPaymentsStorePending: paymentsState.pending,
-            payments: paymentsState.payments
+            payments: paymentsState.payments,
+            filter: FILTERS.ALL_USERS
         };
     },
     componentDidMount: function () {
@@ -40,10 +62,19 @@ var GamesExplorer = React.createClass({
     },
     getTableData: function() {
         var clonesUsersData = _.cloneDeep(this.props.usersData);
-        return _.mapValues(clonesUsersData, function(userData, uid) {
+        var mappedUsers = _.mapValues(clonesUsersData, function(userData, uid) {
             userData.maxSeats = this.state.payments[uid] ? this.state.payments[uid].maxSeats : 0;
             return userData;
         }, this);
+        return filterUsersData(this.state.filter, mappedUsers);
+    },
+    getFilterOptions: function() {
+        return [
+            { payload: FILTERS.ALL_USERS, text: 'כל המשתמשים' },
+            { payload: FILTERS.MEMBERS_ONLY, text: 'מנויים בלבד' },
+            { payload: FILTERS.FREE_ONLY, text: 'חד פעמיים בלבד' },
+            { payload: FILTERS.ASKED_FOR_MEMBERSHIP, text: 'מבקשים לרכוש מנוי' }
+        ];
     },
     onUpdateGame: function(uid, data) {
         var userData = _.pick(data, _.keys(dataSchemas.UserData));
