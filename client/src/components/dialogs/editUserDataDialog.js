e7f0a4b79de8898dca987779c65e64884b758c9c
chene <chene@wix.com>
2015-10-24 18:48:08 +0300

    start designing the edit user dialog

--- client/src/components/dialogs/editUserDataDialog.js
+++ client/src/components/dialogs/editUserDataDialog.js
@@ -2,12 +2,18 @@
 
 var React = require('react');
 var template = require('./editUserDataDialog.rt');
+var muiMixin = require('../mixins/mui-mixin');
 
 var TestDialog =  React.createClass({
-    mixins: [ ],
+    mixins: [ muiMixin, React.addons.LinkedStateMixin],
     getInitialState: function() {
       console.log(this.props);
-        return {};
+        return {
+            displayName: this.props.data.userData.info.displayName,
+            phone: this.props.data.userData.info.phone,
+            email: this.props.data.userData.info.email,
+            errorMsg: ''
+        };
     },
     render: template
 });
