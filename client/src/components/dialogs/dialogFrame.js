d234f3f91c161786e5d71d0c18cc3983eeec151b
chene <chene@wix.com>
2015-11-02 19:53:31 +0200

    continue design the user data dialog

--- client/src/components/dialogs/dialogFrame.js
+++ client/src/components/dialogs/dialogFrame.js
@@ -1,19 +1,20 @@
 'use strict';
 
 var React = require('react');
+var mui = require('material-ui');
 var template = require('./dialogFrame.rt');
 var muiMixin = require('../mixins/mui-mixin');
 
 var actionsCreator = require('../../actions/actionsCreator');
 var actionsConstants = require('../../actions/actionsConstants');
 
-var DialogFrame =  React.createClass({
-    mixins: [ muiMixin ],
-    componentDidMount: function() {
-      this.refs.dialog.show();
+var DialogFrame = React.createClass({
+    mixins: [muiMixin],
+    componentDidMount: function () {
+        this.refs.dialog.show();
     },
-    hideDialog: function() {
-        setTimeout(function() {
+    hideDialog: function () {
+        setTimeout(function () {
             actionsCreator.createAction(actionsConstants.CLOSE_DIALOG, {});
         }, 500);
     },
--- client/src/components/dialogs/editUserDataDialog.js
+++ client/src/components/dialogs/editUserDataDialog.js
@@ -4,6 +4,10 @@ var React = require('react');
 var template = require('./editUserDataDialog.rt');
 var muiMixin = require('../mixins/mui-mixin');
 
+var actionsCreator = require('../../actions/actionsCreator');
+var actionsConstants = require('../../actions/actionsConstants');
+
+
 var TestDialog =  React.createClass({
     mixins: [ muiMixin, React.addons.LinkedStateMixin],
     getInitialState: function() {
@@ -12,9 +16,25 @@ var TestDialog =  React.createClass({
             displayName: this.props.data.userData.info.displayName,
             phone: this.props.data.userData.info.phone,
             email: this.props.data.userData.info.email,
+            maxSeats: this.props.data.userData.seasonTicket.maxSeats,
+            ticketId: this.props.data.userData.seasonTicket.ticketId,
+            smsList: !!this.props.data.userData.distribution.sms,
+            emailList: !!this.props.data.userData.distribution.email,
             errorMsg: ''
         };
     },
+    onSmsListChange: function (value){
+      this.setState({smsList: value});
+    },
+
+    onEmailListChange: function (value){
+        this.setState({emailList: value});
+    },
+
+    updateUserData: function (){
+        console.log('save')
+    },
+
     render: template
 });
 
