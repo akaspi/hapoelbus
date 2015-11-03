6463578d4841b3f03f407a9f22dd28d238b71a60
Amit_Kaspi <akaspi@wix.com>
2015-11-03 09:57:25 +0200

    merge usersDataAPI update functions to single updateUserData

--- client/src/API/usersDataAPI.js
+++ client/src/API/usersDataAPI.js
@@ -11,31 +11,6 @@ var userDataPathsData = [
     { dataKey: 'distribution', path: 'distribution' }
 ];
 
-function getPath(dataKey) {
-    var pathData = _.find(userDataPathsData, { dataKey: dataKey });
-    return pathData && pathData.path;
-}
-
-function updateUserInfo(uid, userInfo) {
-    var path = getPath('info') + '/' + uid;
-    return db.update(path, userInfo);
-}
-
-function updateDistribution(uid, distribution) {
-    var path = getPath('distribution') + '/' + uid;
-    return db.update(path, distribution);
-}
-
-function updateSeasonTicket(uid, seasonTicket) {
-    var path = getPath('seasonTicket') + '/' + uid;
-    return db.update(path, seasonTicket);
-}
-
-function setContactRequest(uid, contactRequest) {
-    var path = getPath('contactRequest') + '/' + uid;
-    return db.set(path, contactRequest);
-}
-
 function getDataForAllUsers() {
     var readPromises = _.map(userDataPathsData, function (pathData) {
         return db.read(pathData.path);
@@ -44,10 +19,11 @@ function getDataForAllUsers() {
     return Promise.all(readPromises)
         .then(function (results) {
             var UIDs = _.keys(results[0]);
-            window.results = results;
             return _.reduce(UIDs, function (accum, uid) {
                 accum[uid] = _.reduce(userDataPathsData, function (accum, pathData, index) {
-                    accum[pathData.dataKey] = results[index][uid];
+                    if (results[index]) {
+                        accum[pathData.dataKey] = results[index][uid];
+                    }
                     return accum;
                 }, {});
                 return accum;
@@ -81,10 +57,19 @@ function getUsersData() {
     });
 }
 
+function updateUserData(uid, userData) {
+    var updatePromises = _.map(userDataPathsData, function(pathData) {
+        if (userData[pathData.dataKey]) {
+            var path =  pathData.path + '/' + uid;
+            var dataToUpdate = userData[pathData.dataKey];
+            return db.update(path, dataToUpdate);
+        }
+    });
+
+    return Promise.all(updatePromises);
+}
+
 module.exports = {
-    updateUserInfo: updateUserInfo,
-    updateDistribution: updateDistribution,
-    updateSeasonTicket: updateSeasonTicket,
-    setContactRequest: setContactRequest,
-    getUsersData: getUsersData
+    getUsersData: getUsersData,
+    updateUserData: updateUserData
 };
