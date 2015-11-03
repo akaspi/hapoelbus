96a0cad7fc20e948ee24d206cff9fd7bf26b86ba
Amit_Kaspi <akaspi@wix.com>
2015-11-03 21:52:40 +0200

    update card design

--- client/src/components/cards/card.js
+++ client/src/components/cards/card.js
@@ -3,7 +3,7 @@
 var React = require('react');
 var template = require('./card.rt');
 
-var Card =  React.createClass({
+var Card = React.createClass({
     propTypes: {
         title: React.PropTypes.string.isRequired,
         subtitles: React.PropTypes.arrayOf(React.PropTypes.string),
@@ -16,17 +16,25 @@ var Card =  React.createClass({
         })),
         imageUrl: React.PropTypes.string,
         ribbon: React.PropTypes.shape({
-            color: React.PropTypes.oneOf(['Green', 'Red']),
+            color: React.PropTypes.oneOf(['Green', 'Red', 'Blue']),
             label: React.PropTypes.string.isRequired
         })
     },
-    getInitialState: function() {
+    getInitialState: function () {
         return {};
     },
-    onCardEdit: function() {
-      if (_.isFunction(this.props.onEdit)) {
-          this.props.onEdit(this.props.id);
-      }
+    getRibbonClasses: function () {
+        return {
+            "card-ribbon": true,
+            "ribbon-green": this.props.ribbon.color === 'Green',
+            "ribbon-red": this.props.ribbon.color === 'Red',
+            "ribbon-blue": this.props.ribbon.color === 'Blue'
+        };
+    },
+    onCardEdit: function () {
+        if (_.isFunction(this.props.onEdit)) {
+            this.props.onEdit(this.props.id);
+        }
     },
     render: template
 });
