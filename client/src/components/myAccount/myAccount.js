f5140dd4ebca737060a9b05ae9d4edc1d9887bef
chene <chene@wix.com>
2015-11-05 19:41:53 +0200

    add header to the my account page

--- client/src/components/myAccount/myAccount.js
+++ client/src/components/myAccount/myAccount.js
@@ -6,19 +6,23 @@ var template = require('./myAccount.rt');
 var actionsCreator = require('../../actions/actionsCreator');
 var actionsConstants = require('../../actions/actionsConstants');
 
-var authAPI = require('../../API/authAPI');
+//var authAPI = require('../../API/authAPI');
 
-var editUserDataDialog = require('../dialogs/editUserDataDialog');
+//var editUserDataDialog = require('../dialogs/editUserDataDialog');
 
 var MyAccount = React.createClass({
     componentDidMount: function () {
-        if (_.isEmpty(this.props.usersData)) {
-            actionsCreator.createAction(actionsConstants.SHOW_DIALOG, {
-                dialogClass: editUserDataDialog,
-                data: {showCancelButton: false, isModal: true, createMode: true, uid: authAPI.getUID(), userData: {info: {email: authAPI.getUserEmail()}}}
-            });
-        }
+        //if (_.isEmpty(this.props.usersData)) {
+        //    actionsCreator.createAction(actionsConstants.SHOW_DIALOG, {
+        //        dialogClass: editUserDataDialog,
+        //        data: {showCancelButton: false, isModal: true, createMode: true, uid: authAPI.getUID(), userData: {info: {email: authAPI.getUserEmail()}}}
+        //    });
+        //}
     },
+    logout: function() {
+        actionsCreator.createAction(actionsConstants.LOGOUT, {});
+    },
+
     render: template
 });
 
