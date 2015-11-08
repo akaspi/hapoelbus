a59edfdb53a8582169c0df989739cec2483ef760
chene <chene@wix.com>
2015-11-08 19:37:39 +0200

    games design

--- client/src/components/cards/card.js
+++ client/src/components/cards/card.js
@@ -6,10 +6,10 @@ var template = require('./card.rt');
 var Card = React.createClass({
     propTypes: {
         title: React.PropTypes.string.isRequired,
-        subtitles: React.PropTypes.arrayOf(React.PropTypes.string),
-        onEdit: React.PropTypes.func,
-        onRemove: React.PropTypes.func,
-        customActions: React.PropTypes.arrayOf(React.PropTypes.shape({
+        subtitles: React.PropTypes.arrayOf(React.PropTypes.string, React.PropTypes.number),
+        //onEdit: React.PropTypes.func,
+        //onRemove: React.PropTypes.func,
+        actions: React.PropTypes.arrayOf(React.PropTypes.shape({
             onClick: React.PropTypes.func,
             label: React.PropTypes.string,
             iconClass: React.PropTypes.string
@@ -26,16 +26,6 @@ var Card = React.createClass({
                 return 'card-green';
             case 'Blue':
                 return 'card-blue';
-        }
-    },
-    onCardEdit: function () {
-        if (_.isFunction(this.props.onEdit)) {
-            this.props.onEdit(this.props.id);
-        }
-    },
-    onCardRemove: function () {
-        if (_.isFunction(this.props.onRemove)) {
-            this.props.onRemove(this.props.id);
         }
     },
     render: template
