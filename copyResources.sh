e4581072b72a28ede6776492c2533b7b2f2c2892
Amit_Kaspi <akaspi@wix.com>
2015-11-24 22:21:03 +0200

    fix build flow

--- copyResources.sh
+++ copyResources.sh
@@ -3,5 +3,4 @@
 PUBLIC_DIR="./client/public"
 RESOURCES_DIR="./client/resources"
 
-cp $RESOURCES_DIR/index.html $PUBLIC_DIR/
-cp $RESOURCES_DIR/favicon.ico $PUBLIC_DIR/
+mkdir -p $PUBLIC_DIR/ && cp $RESOURCES_DIR/index.html $PUBLIC_DIR/ && cp $RESOURCES_DIR/favicon.ico $PUBLIC_DIR/
