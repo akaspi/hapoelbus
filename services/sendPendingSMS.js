47ee6b101099a18ef0ffb641798b5fc4be8760ac
Amit_Kaspi <akaspi@wix.com>
2015-12-05 21:43:16 +0200

    add concurrency parameter to pendingSMS service

--- services/sendPendingSMS.js
+++ services/sendPendingSMS.js
@@ -9,60 +9,64 @@ var accountSid = 'AC297f818312918ead6e0d8f67f7d0f313';
 var authToken = 'd3d7fb41dd37955d1c3dd41ddb82df9f';
 var smsClient = require('twilio')(accountSid, authToken);
 
+var CONCURRENCY_SMS_CALLS = 10;
+
 var PENDING_SMS_PATH = 'pendingSMS';
 
 function sendSMS(phoneNumber, content) {
-    return new Promise(function(resolve, reject) {
-        smsClient.messages.create({
-            from: 'HapoelBus',
-            to: phoneNumber,
-            body: content
-        }, function(err, message) {
-            if (err) { return reject(err) }
-            resolve(message.id);
-        });
+    return new Promise(function (resolve, reject) {
+        setTimeout(function() {
+            smsClient.messages.create({
+                from: 'HapoelBus',
+                to: phoneNumber,
+                body: content
+            }, function (err, message) {
+                if (err) {
+                    return reject(err)
+                }
+                resolve(message.id);
+            });
+        }, 10000);
     });
 }
 
 function sendPendingTemplate(pendingTemplate, smsId) {
-    var to = pendingTemplate.to || [];
+    var to =  pendingTemplate.to || [];
     var content = templateUtils.getSMSContent(pendingTemplate.templateId, pendingTemplate.subs);
 
-    var sendSMSPromises = _.map(to, function(phoneNumber) {
-        return sendSMS(phoneNumber, content);
-    });
-
-    return Promise.all(sendSMSPromises).then(function() {
-        return dbUtils.remove(PENDING_SMS_PATH + '/templates/' + smsId);
-    });
+    return Promise.map(to, function (phoneNumber) {
+            return sendSMS(phoneNumber, content);
+        }, {concurrency: CONCURRENCY_SMS_CALLS})
+        .then(function () {
+            return dbUtils.remove(PENDING_SMS_PATH + '/templates/' + smsId);
+        })
 }
 
 function sendPendingCustom(pendingCustom, smsId) {
     var to = pendingCustom.to || [];
     var content = pendingCustom.content || '';
 
-    var sendSMSPromises = _.map(to, function(phoneNumber) {
-        return sendSMS(phoneNumber, content);
-    });
-
-    return Promise.all(sendSMSPromises).then(function() {
-        return dbUtils.remove(PENDING_SMS_PATH + '/custom/' + smsId)
-    });
+    return Promise.map(to, function (phoneNumber) {
+            return sendSMS(phoneNumber, content);
+        }, {concurrency: CONCURRENCY_SMS_CALLS})
+        .then(function () {
+            return dbUtils.remove(PENDING_SMS_PATH + '/custom/' + smsId);
+        });
 }
 
 console.log('\n\n****** RUNS AT ' + new Date() + ' ******');
 dbUtils.loginAsAdmin()
-    .then(function() {
+    .then(function () {
         return dbUtils.read(PENDING_SMS_PATH)
     })
-    .then(function(pendingSMS) {
+    .then(function (pendingSMS) {
         var promises = [];
 
-        _.forOwn(pendingSMS.templates, function(pendingTemplate, smsId) {
+        _.forOwn(pendingSMS.templates, function (pendingTemplate, smsId) {
             promises.push(sendPendingTemplate(pendingTemplate, smsId));
         });
 
-        _.forOwn(pendingSMS.custom, function(pendingCustom, smsId) {
+        _.forOwn(pendingSMS.custom, function (pendingCustom, smsId) {
             promises.push(sendPendingCustom(pendingCustom, smsId));
         });
 
@@ -73,12 +77,12 @@ dbUtils.loginAsAdmin()
 
         return Promise.all(promises);
     })
-    .then(function() {
+    .then(function () {
         console.log('All SMS were sent successfully');
     })
-    .catch(function(e) {
+    .catch(function (e) {
         console.log(e.stack);
     })
-    .finally(function() {
+    .finally(function () {
         process.exit();
     });
