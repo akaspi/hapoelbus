162f6bd2a9a87fd85258dc3b1866bd19d7fbdd58
Amit_Kaspi <akaspi@wix.com>
2016-06-23 09:44:04 +0300

    add eslint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,17 @@
 module.exports = function (grunt) {
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        jasmine: {
-          all: ['src/**/*.spec.js']
-        }
-    });
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jasmine: {
+      all: ['src/**/*.spec.js']
+    },
+    eslint: {
+      target: ['.']
+    }
+  });
 
-    grunt.loadNpmTasks('grunt-jasmine-npm');
+  grunt.loadNpmTasks('grunt-jasmine-npm');
+  grunt.loadNpmTasks('grunt-eslint');
 
-    grunt.registerTask('default', ['jasmine']);
+  grunt.registerTask('default', ['eslint', 'jasmine']);
+  grunt.registerTask('lint', ['eslint']);
 };
