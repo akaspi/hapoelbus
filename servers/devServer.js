cef025c66fede92bd98a96cd5e42c5521ac0c59c
Amit_Kaspi <akaspi@wix.com>
2016-06-24 11:28:50 +0300

    create dev server using webpack-dev-server

---
+++ servers/devServer.js
@@ -0,0 +1,20 @@
+const webpack =require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+const webpackConfig = require('../webpack.config');
+const _  = require('lodash');
+
+const webpackDevConfig = _.cloneDeep(webpackConfig);
+const devPort = 3000;
+
+webpackDevConfig.entry.unshift('webpack/hot/dev-server');
+webpackDevConfig.entry.unshift('webpack-dev-server/client?http://localhost:' + devPort + '/');
+webpackDevConfig.plugins.push(new webpack.HotModuleReplacementPlugin());
+
+new WebpackDevServer(webpack(webpackDevConfig), {
+  publicPath: webpackConfig.output.publicPath,
+  hot: true,
+  stats: {
+    colors: true,
+    chunks: false
+  }
+}).listen(devPort, 'localhost');
