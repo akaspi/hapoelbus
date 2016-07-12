bedfff6e88a45d4b209bff057c62fa73d75d9af3
Amit_Kaspi <akaspi@wix.com>
2016-07-12 21:39:27 +0300

    create DatabaseAPI

---
+++ common/DatabaseAPI.js
@@ -0,0 +1,29 @@
+const firebase = require('firebase');
+
+const createAPI = (config) => {
+  const firebaseApp = firebase.initializeApp(config);
+  const database = firebaseApp.database();
+
+  return {
+    setIn(path, data) {
+      return database.ref(path).set(data);
+    },
+    read(path) {
+      return database.ref(path).once('value').then(snapshot => snapshot.val());
+    },
+    push(path, data) {
+      const uniqueKey = database.ref(path).push().key;
+      return this.setIn(path + '/' + uniqueKey, data);
+    },
+    update(path, data) {
+      return database.ref().update({ [path]: data });
+    },
+    remove(path) {
+      return database.ref(path).remove();
+    }
+  };
+};
+
+module.exports = {
+  init: createAPI
+};
