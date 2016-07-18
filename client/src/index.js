db76f5e3beca0e86b056eeda7404d819e2a64c89
Amit_Kaspi <akaspi@wix.com>
2016-07-18 20:27:53 +0300

    add HMR hack

--- client/src/index.js
+++ client/src/index.js
@@ -3,3 +3,8 @@ import ReactDOM from 'react-dom';
 import Root from './components/root/root';
 
 ReactDOM.render(React.createElement(Root), document.getElementById('app'));
+
+// HMR hack. should be fixed somehow...
+if (module.hot) {
+  module.hot.accept();
+}
