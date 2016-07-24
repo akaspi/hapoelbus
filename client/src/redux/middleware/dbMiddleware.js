f0aa7cca95319bbe83c87a1070c1c308d06d0fc8
Amit_Kaspi <akaspi@wix.com>
2016-07-24 11:27:18 +0300

    DatabaseAPI wrapper

--- client/src/redux/middleware/dbMiddleware.js
+++ client/src/redux/middleware/dbMiddleware.js
@@ -1,9 +1,16 @@
 import * as _ from 'lodash';
-import clientConfig from '../../../../conf/client.config.json';
-import firebase from 'firebase';
 import Constants from '../../utils/constants';
+import DatabaseAPI from '../../../../common/DatabaseAPI';
+import * as clientConfig from '../../../../conf/client.config.json';
 
-import { setCurrentUser, updateUserInfo, startLoading, endLoading, reportError } from '../actions/actionsCreator';
+import {
+  setCurrentUser,
+  updateUserInfo,
+  startLoading,
+  endLoading,
+  reportError
+} from '../actions/actionsCreator';
+
 import {
   LOGIN_WITH_GOOGLE,
   LOGIN_WITH_FACEBOOK,
@@ -14,7 +21,9 @@ import {
   SIGN_OUT
 } from '../actions/actionTypes';
 
+const dbAPI = new DatabaseAPI(clientConfig.firebase);
 const CURRENT_USER_KEYS = ['uid', 'email'];
+
 const errorCodeMap = {
   'auth/invalid-email': Constants.ERRORS.INVALID_MAIL,
   'auth/weak-password': Constants.ERRORS.WEAK_PASSWORD,
@@ -23,103 +32,86 @@ const errorCodeMap = {
 
 const parseDBError = errorCode => (errorCodeMap[errorCode] || Constants.ERRORS.GENERAL);
 
-const asyncEnd = (next, cb) => (response) => {
-  next(endLoading());
-  cb(response);
-};
-
-const loginWithProvider = (provider, onSuccess, onError) => {
-  firebase.auth().signInWithPopup(provider)
-    .then(result => onSuccess(_.pick(result.user, CURRENT_USER_KEYS)))
-    .catch(error => onError(error));
-};
-
-const loginWithFacebook = next => {
-  const facebookProvider = new firebase.auth.FacebookAuthProvider();
-  const onLoginSuccess = (currentUser) => next(setCurrentUser(currentUser));
-  const onLoginError = error => next(reportError(error.message));
-
-  loginWithProvider(facebookProvider, onLoginSuccess, onLoginError);
-};
-
-const loginWithGoogle = next => {
-  const facebookProvider = new firebase.auth.GoogleAuthProvider();
-  const onLoginSuccess = (currentUser) => next(setCurrentUser(currentUser));
-  const onLoginError = error => next(reportError(error.message));
-
-  loginWithProvider(facebookProvider, onLoginSuccess, onLoginError);
-};
-
-const signUpWithEmailAndPassword = (next, action) => {
-  const onSignUpSuccess = (user) => next(setCurrentUser(_.pick(user, CURRENT_USER_KEYS)));
-  const onSignUpError = (error) => next(reportError(parseDBError(error.code)));
-
-  next(startLoading());
-  firebase.auth().createUserWithEmailAndPassword(action.email, action.password)
-    .then(asyncEnd(next, onSignUpSuccess))
-    .catch(asyncEnd(next, onSignUpError));
-};
-
-const dbUpdateUserInfo = (next, action) => {
-  const onUpdateComplete = (error) => {
-    if (error) {
-      return next(reportError(error.message));
-    }
-    return next(action);
+const loginWithFacebook = (action, next, onSuccess, onError) => {
+  const onLoginSuccess = user => {
+    next(setCurrentUser(_.pick(user, CURRENT_USER_KEYS)));
+    onSuccess();
   };
 
-  next(startLoading());
-  firebase.database().ref('usersInfo/' + action.uid).update(action.userInfo, asyncEnd(next, onUpdateComplete));
+  dbAPI.loginWithFacebook(onLoginSuccess, onError);
 };
 
-const dbSignOut = (next, action) => {
-  firebase.auth().signOut().then(
-    () => next(action),
-    error => next(reportError(error.message))
-  );
+const loginWithGoogle = (action, next, onSuccess, onError) => {
+  const onLoginSuccess = user => {
+    next(setCurrentUser(_.pick(user, CURRENT_USER_KEYS)));
+    onSuccess();
+  };
+
+  dbAPI.loginWithGoogle(onLoginSuccess, onError);
 };
 
-const fetchUserInfo = (next, action) => {
-  const onFetchSuccess = snapshot => next(updateUserInfo(action.uid, snapshot.val()));
-  const onFetchError = error => next(reportError(error.message));
+const signUpWithEmailAndPassword = (action, next, onSuccess, onError) => {
+  const onSignUpSuccess = user => {
+    next(setCurrentUser(user));
+    onSuccess();
+  };
 
-  next(startLoading());
-  firebase.database().ref('usersInfo/' + action.uid).once('value', asyncEnd(next, onFetchSuccess), asyncEnd(next, onFetchError));
+  dbAPI.createUserWithEmailAndPassword(action.email, action.password, onSignUpSuccess, onError);
 };
 
-const fetchCurrentUser = (next) => {
-  const onAuthStateChange = (user) => {
-    next(endLoading());
+const dbUpdateUserInfo = (action, next, onSuccess, onError) => {
+  const userInfoPath = 'usersInfo/' + action.uid;
+  dbAPI.update(userInfoPath, action.userInfo, onSuccess, onError);
+};
+
+const signOut = (next, action, onSuccess, onError) => {
+  dbAPI.signOut(onSuccess, onError);
+};
+
+const fetchUserInfo = (action, next, onSuccess, onError) => {
+  const userInfoPath = 'usersInfo/' + action.uid;
+  const onFetchSuccess = userInfo => {
+    next(updateUserInfo(action.uid, userInfo));
+    onSuccess();
+  };
+
+  dbAPI.read(userInfoPath, onFetchSuccess, onError);
+};
+
+const fetchCurrentUser = (action, next, onSuccess) => {
+  dbAPI.getLoggedInUser(user => {
     if (user) {
       next(setCurrentUser(_.pick(user, CURRENT_USER_KEYS)));
+      onSuccess();
     }
-    firebase.auth().removeAuthTokenListener(onAuthStateChange);
-  };
-
-  next(startLoading());
-  firebase.auth().onAuthStateChanged(onAuthStateChange);
+  });
 };
 
-firebase.initializeApp(clientConfig.firebase);
-window.fb = firebase;
+const actionsMap = {
+  [LOGIN_WITH_FACEBOOK]: loginWithFacebook,
+  [LOGIN_WITH_GOOGLE]: loginWithGoogle,
+  [SIGN_UP_WITH_EMAIL_AND_PASSWORD]: signUpWithEmailAndPassword,
+  [SIGN_OUT]: signOut,
+  [FETCH_CURRENT_USER]: fetchCurrentUser,
+  [FETCH_USER_INFO]: fetchUserInfo,
+  [UPDATE_USER_INFO]: dbUpdateUserInfo
+};
+
+const onActionStart = next => {
+  next(startLoading());
+  next(reportError(''));
+};
+const onActionSuccess = next => next(endLoading());
+const onActionError = (next, dbError) => {
+  next(endLoading());
+  next(reportError(parseDBError(dbError.code)));
+};
 
 export default store => next => action => { // eslint-disable-line no-unused-vars
-  switch (action.type) {
-    case LOGIN_WITH_FACEBOOK:
-      return loginWithFacebook(next);
-    case LOGIN_WITH_GOOGLE:
-      return loginWithGoogle(next);
-    case SIGN_UP_WITH_EMAIL_AND_PASSWORD:
-      return signUpWithEmailAndPassword(next, action);
-    case SIGN_OUT:
-      return dbSignOut(next, action);
-    case FETCH_CURRENT_USER:
-      return fetchCurrentUser(next);
-    case FETCH_USER_INFO:
-      return fetchUserInfo(next, action);
-    case UPDATE_USER_INFO:
-      return dbUpdateUserInfo(next, action);
-    default:
-      return next(action);
+  if (_.isFunction(actionsMap[action.type])) {
+    onActionStart(next);
+    actionsMap[action.type](action, next, onActionSuccess.bind(null, next), onActionError.bind(null, next));
   }
+
+  return next(action);
 };
--- common/DatabaseAPI.js
+++ common/DatabaseAPI.js
@@ -1,39 +1,83 @@
+const _ = require('lodash');
 const firebase = require('firebase');
-const createAPI = (config) => {
-  const firebaseApp = firebase.initializeApp(config);
-  const database = firebaseApp.database();
 
-  return {
-    setIn(path, data) {
-      return database.ref(path).set(data);
-    },
-    read(path) {
-      return database.ref(path).once('value').then(snapshot => snapshot.val());
-    },
-    push(path, data) {
-      const uniqueKey = database.ref(path).push().key;
-      return this.setIn(path + '/' + uniqueKey, data);
-    },
-    update(path, data) {
-      return database.ref().update({ [path]: data });
-    },
-    remove(path) {
-      return database.ref(path).remove();
-    },
-    loginWithGoogle() {
-      const provider = new firebase.auth.GoogleAuthProvider();
-      return firebaseApp.auth().signInWithPopup(provider);
-    },
-    loginWithFacebook() {
-      const provider = new firebase.auth.FacebookAuthProvider();
-      return firebaseApp.auth().signInWithPopup(provider);
-    },
-    createUserWithEmailAndPassword(email, password) {
-      return firebaseApp.auth().createUserWithEmailAndPassword(email, password);
-    }
-  };
-};
+class DatabaseAPI {
+  constructor(config) {
+    this.firebaseApp = firebase.initializeApp(config);
+  }
 
-module.exports = {
-  init: createAPI
-};
+  setIn(path, data, onSuccess, onError) {
+    this.firebaseApp.database().ref(path).set(data, error => {
+      if (error) {
+        return onError(error);
+      }
+      return onSuccess();
+    });
+  }
+
+  read(path, onSuccess, onError) {
+    const onReadSuccess = (snapshot) => onSuccess(snapshot.val());
+    const onReadError = (error) => onError(error);
+    this.firebaseApp.database().ref(path).once('value', onReadSuccess, onReadError);
+  }
+
+  push(path, data, onSuccess, onError) {
+    const uniqueKey = this.firebaseApp.database().ref(path).push().key;
+    this.setIn(path + '/' + uniqueKey, data, onSuccess, onError);
+  }
+
+  update(path, data, onSuccess, onError) {
+    this.firebaseApp.database().ref(path).update(data, error => {
+      if (error) {
+        return onError(error);
+      }
+      return onSuccess();
+    });
+  }
+
+  remove(path, onSuccess, onError) {
+    this.firebaseApp.database().ref(path).remove(error => {
+      if (error) {
+        return onError(error);
+      }
+      return onSuccess();
+    });
+  }
+
+  loginWithGoogle(onSuccess, onError) {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    this.firebaseApp.auth().signInWithPopup(provider)
+      .then(result => onSuccess(result.user))
+      .catch(error => onError(error));
+  }
+
+  loginWithFacebook(onSuccess, onError) {
+    const provider = new firebase.auth.FacebookAuthProvider();
+    this.firebaseApp.auth().signInWithPopup(provider)
+      .then(result => onSuccess(result.user))
+      .catch(error => onError(error));
+  }
+
+  createUserWithEmailAndPassword(email, password, onSuccess, onError) {
+    this.firebaseApp.auth().createUserWithEmailAndPassword(email, password)
+      .then(user => onSuccess(_.pick(user, ['uid', 'email'])))
+      .catch(error => onError(error));
+  }
+
+  signOut(onSuccess, onError) {
+    this.firebaseApp.auth().signOut()
+      .then(() => onSuccess())
+      .catch(error => onError(error));
+  }
+
+  getLoggedInUser(onComplete) {
+    const onAuthStateChange = user => {
+      firebase.auth().removeAuthTokenListener(onAuthStateChange);
+      onComplete(user);
+    };
+    firebase.auth().onAuthStateChanged(onAuthStateChange);
+  }
+
+}
+
+module.exports = DatabaseAPI;
