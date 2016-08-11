c5996c99db18f4c978f2fcbe3a634c87313fcb1e
Amit_Kaspi <akaspi@wix.com>
2016-08-11 21:16:58 +0300

    createSchedulerTasks grunt task

--- conf/grunt/Gruntfile.js
+++ conf/grunt/Gruntfile.js
@@ -1,5 +1,4 @@
 const webpackConfig = require('../../webpack.config');
-const _ = require('lodash');
 
 module.exports = function (grunt) {
   grunt.initConfig({
@@ -22,28 +21,6 @@ module.exports = function (grunt) {
     webpack: {
       production: webpackConfig
     },
-    copy: {
-      schedulerTasks: {
-        options: {
-          process: (content) => {
-            const sheband = '#!/usr/bin/env node';
-            return sheband + '\n\n' + content;
-          }
-        },
-        files: [
-          {
-            expand: true,
-            src: ['server/tasks/**/*'],
-            dest: 'bin/',
-            rename: (dest, src) => {
-              const fileName = _.last(src.split('/'));
-              const fileNameWithoutExtension = _.first(fileName.split('.'));
-              return dest + fileNameWithoutExtension;
-            }
-          }
-        ]
-      }
-    },
     clean: {
       dist: ['client/dist'],
       bin: ['bin'],
@@ -54,14 +31,14 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-jasmine-npm');
   grunt.loadNpmTasks('grunt-eslint');
   grunt.loadNpmTasks('grunt-webpack-without-server');
-  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-clean');
 
-  grunt.registerTask('schedulerTasks', ['clean:bin', 'copy:schedulerTasks']);
+  grunt.registerTask('schedulerTasks', ['clean:bin', 'createSchedulerTasks']);
   grunt.registerTask('buildWebpack', ['clean:dist', 'webpack:production']);
   grunt.registerTask('buildConfig', ['clean:config', 'createConfigFiles']);
   grunt.registerTask('test', ['jasmine']);
   grunt.registerTask('lint', ['eslint']);
+
   grunt.registerTask('build', ['buildConfig', 'buildWebpack', 'schedulerTasks']);
 
   grunt.task.loadTasks('./conf/grunt/tasks/');
