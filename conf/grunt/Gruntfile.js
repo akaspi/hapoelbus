a537f1d4047c751bf035de08aef166386680ec5c
Amit_Kaspi <akaspi@wix.com>
2016-08-14 12:07:06 +0300

    split client / server Grunt test tasks

--- conf/grunt/Gruntfile.js
+++ conf/grunt/Gruntfile.js
@@ -4,15 +4,23 @@ module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     jasmine: {
-      all: {
+      client: {
         src: [
           // helpers
           'node_modules/babel-register/lib/node.js',
           'client/test/helpers/**/*.js',
+
+          // specs
+          'client/**/*.spec.js'
+        ]
+      },
+      server: {
+        src: [
+          // helpers
+          'node_modules/babel-register/lib/node.js',
           'server/test/helpers/**/*.js',
 
           // specs
-          'client/**/*.spec.js',
           'server/**/*.spec.js'
         ]
       }
@@ -38,12 +46,15 @@ module.exports = function (grunt) {
   grunt.registerTask('schedulerTasks', ['clean:bin', 'createSchedulerTasks']);
   grunt.registerTask('buildWebpack', ['clean:dist', 'webpack:production']);
   grunt.registerTask('buildConfig', ['clean:config', 'createConfigFiles']);
-  grunt.registerTask('test', ['jasmine']);
+
+  grunt.registerTask('test:client', ['jasmine:client']);
+  grunt.registerTask('test:server', ['jasmine:server']);
+
   grunt.registerTask('lint', ['eslint']);
 
   grunt.registerTask('build', ['buildConfig', 'buildWebpack', 'schedulerTasks']);
 
   grunt.task.loadTasks('./conf/grunt/tasks/');
 
-  grunt.registerTask('default', ['lint', 'test', 'build']);
+  grunt.registerTask('default', ['lint', 'test:client', 'test:server', 'build']);
 };
