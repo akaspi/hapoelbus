7c2521f3be727494b117085578ca869d6a11266a
chene <chene@wix.com>
2016-08-29 19:46:14 +0300

    formFrame

---
+++ client/src/components/formFrame/formFrame.js
@@ -0,0 +1,24 @@
+import * as _ from 'lodash';
+import React from 'react';
+import template from './formFrame.rt';
+
+class FormFrame extends React.Component {
+  onSubmit() {
+    this.props.onSubmit();
+    this.props.onClose();
+  }
+
+  render() {
+    return template.apply(this);
+  }
+}
+
+FormFrame.propTypes = {
+  onSubmit: React.PropTypes.func.isRequired,
+  onClose: React.PropTypes.func.isRequired,
+  title: React.PropTypes.string,
+  disabled: React.PropTypes.bool,
+  hideBack: React.PropTypes.bool
+};
+
+module.exports = FormFrame;
