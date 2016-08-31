81ef39080836517b4ace49cb976eacb06304e04b
Amit_Kaspi <akaspi@wix.com>
2016-08-31 14:55:18 +0300

    add remove event action

--- client/src/redux/reducers/eventsReducer.js
+++ client/src/redux/reducers/eventsReducer.js
@@ -1,5 +1,5 @@
 import * as _ from 'lodash';
-import { EVENTS_RECEIVED, SIGN_OUT } from '../actions/actionTypes';
+import { EVENTS_RECEIVED, EVENT_REMOVED, SIGN_OUT } from '../actions/actionTypes';
 
 const initialState = {};
 
@@ -7,6 +7,8 @@ export default function eventsReducer(state = initialState, action = {}) {
   switch (action.type) {
     case EVENTS_RECEIVED:
       return _.merge({}, state, action.events);
+    case EVENT_REMOVED:
+      return _.omit(state, action.eventId);
     case SIGN_OUT:
       return initialState;
     default:
