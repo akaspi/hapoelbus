da816bfde04e368bf68bc9f2971d7e3c40d9e033
Amit_Kaspi <akaspi@wix.com>
2016-09-01 17:36:17 +0300

    eventsReducer tests

---
+++ client/test/eventsReducer.spec.js
@@ -0,0 +1,81 @@
+import eventsReducer from '../src/redux/reducers/eventsReducer';
+import { eventsReceived, eventRemoved } from '../src/redux/actions/eventActions';
+import { userSignedOut } from '../src/redux/actions/authActions';
+
+describe('eventsReducer spec', () => {
+
+  it('should be empty by default', () => {
+    const nextState = eventsReducer();
+
+    expect(nextState).toEqual({});
+  });
+
+  it('should merge received events', () => {
+    const currentState = {
+      eventId: {
+        name: 'EVENT A'
+      }
+    };
+
+    const nextState = eventsReducer(currentState, eventsReceived({
+      eventId: {
+        month: '04'
+      },
+      eventId2: {
+        name: 'EVENT B',
+        month: '02'
+      }
+    }));
+
+    expect(nextState).toEqual({
+      eventId: {
+        name: 'EVENT A',
+        month: '04'
+      },
+      eventId2: {
+        name: 'EVENT B',
+        month: '02'
+      }
+    });
+  });
+
+  it('should remove event', () => {
+    const currentState = {
+      eventId: {
+        name: 'EVENT A',
+        month: '04'
+      },
+      eventId2: {
+        name: 'EVENT B',
+        month: '02'
+      }
+    };
+
+    const nextState = eventsReducer(currentState, eventRemoved('eventId'));
+
+    expect(nextState).toEqual({
+      eventId2: {
+        name: 'EVENT B',
+        month: '02'
+      }
+    });
+  });
+
+  it('should be empty after sign out', () => {
+    const currentState = {
+      eventId: {
+        name: 'EVENT A',
+        month: '04'
+      },
+      eventId2: {
+        name: 'EVENT B',
+        month: '02'
+      }
+    };
+
+    const nextState = eventsReducer(currentState, userSignedOut());
+
+    expect(nextState).toEqual({});
+  });
+
+});
