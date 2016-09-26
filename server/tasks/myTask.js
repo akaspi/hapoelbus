ce2cd9a64d1c03fc7cbfefa38e05e7f161cb56c4
Amit_Kaspi <akaspi@wix.com>
2016-09-26 18:28:35 +0300

    add promise to myTask

--- server/tasks/myTask.js
+++ server/tasks/myTask.js
@@ -1,7 +1,11 @@
+const Promise = require('bluebird');
 const config = require('../../conf/server.config.json');
 
 const exec = () => {
-  console.log(config.firebase.serviceAccount.privateKey);
+  return Promise.resolve()
+    .then(() => {
+      console.log(config.firebase.serviceAccount.privateKey);
+    });
 };
 
 module.exports = { exec };
