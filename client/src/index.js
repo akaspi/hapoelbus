e8a6a1571ccff29571cbecf10f671485b4e8dbe8
Amit_Kaspi <akaspi@wix.com>
2016-09-27 19:47:24 +0300

    npm run dev with webpack-dev-server and hot reload

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,3 @@ import ReactDOM from 'react-dom';
 import Root from './components/root/root';
 
 ReactDOM.render(React.createElement(Root), document.getElementById('app'));
-
-// HMR hack. should be fixed somehow...
-if (module.hot) {
-  module.hot.accept();
-}
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -3,7 +3,13 @@ const webpack = require('webpack');
 module.exports = {
   cache: true,
 
-  entry: './client/src/index.js',
+  devtool: 'eval',
+
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './client/src/index.js'
+  ],
 
   output: {
     path: __dirname + '/dist',
@@ -13,12 +19,17 @@ module.exports = {
 
   module: {
     loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loaders: ['babel'] },
+      { test: /\.js$/, exclude: /node_modules/, loaders: ['react-hot-loader', 'babel'] },
       { test: /\.json$/, exclude: /node_modules/, loaders: ['json'] },
       { test: /\.rt/, exclude: /node_modules/, loaders: ['react-templates-loader'] },
       { test: /\.scss/, exclude: /node_modules/, loaders: ['style', 'css', 'sass'] },
       { test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$/, exclude: /node_modules/, loaders: ['file'] }
     ]
+  },
+
+  devServer: {
+    contentBase: './dist',
+    hot: true
   }
 
 };
