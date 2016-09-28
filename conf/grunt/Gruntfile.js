940b94615f0d51f190126ba4783302ce206ed5c9
Amit_Kaspi <akaspi@wix.com>
2016-09-28 17:25:17 +0300

    remove eslint from grunt

--- conf/grunt/Gruntfile.js
+++ conf/grunt/Gruntfile.js
@@ -1,25 +1,18 @@
 module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    eslint: {
-      target: ['.']
-    },
     clean: {
       bin: ['bin']
     }
   });
 
-  // grunt.loadNpmTasks('grunt-jasmine-npm');
-  grunt.loadNpmTasks('grunt-eslint');
   grunt.loadNpmTasks('grunt-contrib-clean');
 
   grunt.registerTask('schedulerTasks', ['clean:bin', 'createSchedulerTasks']);
-
-  grunt.registerTask('lint', ['eslint']);
 
   grunt.registerTask('build', ['schedulerTasks']);
 
   grunt.task.loadTasks('./conf/grunt/tasks/');
 
-  grunt.registerTask('default', ['lint', 'build']);
+  grunt.registerTask('default', ['build']);
 };
