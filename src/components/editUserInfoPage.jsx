41249050e2080e72d91db4dd595ec4853ebccb74
Amit_Kaspi <akaspi@wix.com>
2016-12-13 18:08:19 +0200

    editUserInfoPage.jsx

---
+++ src/components/editUserInfoPage.jsx
@@ -0,0 +1,38 @@
+const React = require('react');
+const ReactRedux = require('react-redux');
+
+const UserForm = require('./userForm.jsx');
+
+const navigationActions = require('../redux/actions/navigationActions');
+const navigationConstants = require('../utils/navigationConstants');
+
+function mapStateToProps(state) {
+    return {
+        uid: state.authData.uid
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        navigateTo: pageId => dispatch(navigationActions.navigateTo(pageId))
+    };
+}
+
+class EditorUserInfoPage extends React.Component {
+    onFormClose = () => {
+        this.props.navigateTo(navigationConstants.PAGES.HOME.val);
+    };
+
+    render() {
+        return (
+            <UserForm uid={this.props.uid} onClose={this.onFormClose}/>
+        )
+    }
+}
+
+EditorUserInfoPage.propTypes = {
+    uid: React.PropTypes.string.isRequired,
+    navigateTo: React.PropTypes.func.isRequired
+};
+
+module.exports = ReactRedux.connect(mapStateToProps, mapDispatchToProps)(EditorUserInfoPage);
