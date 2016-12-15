543f0fe37a5c4cc91a3bece48e36a94eda08e380
Amit_Kaspi <akaspi@wix.com>
2016-12-15 12:53:45 +0200

    create publish npm task

---
+++ webpack.prod.docs.config.js
@@ -0,0 +1,31 @@
+const webpack = require('webpack');
+
+module.exports = {
+  cache: true,
+
+  devtool: 'inline-source-map',
+
+  entry: './src/index.js',
+
+  output: {
+    path: __dirname + '/docs',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loaders: ['react-hot-loader', 'babel'] },
+      { test: /\.jsx$/, exclude: /node_modules/, loaders: ['react-hot-loader', 'babel'] },
+      { test: /\.json$/, exclude: /node_modules/, loaders: ['json'] },
+      { test: /\.rt/, exclude: /node_modules/, loaders: ['react-templates-loader?modules=amd'] },
+      { test: /\.scss/, exclude: /node_modules/, loaders: ['style', 'css', 'sass'] },
+      { test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$/, exclude: /node_modules/, loaders: ['file'] }
+    ]
+  },
+
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  }
+
+};
