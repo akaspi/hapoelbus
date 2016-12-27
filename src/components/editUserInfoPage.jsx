9ce9884879a43e707a3dc91845e39b84695c38dd
Amit_Kaspi <akaspi@wix.com>
2016-12-27 15:51:54 +0200

    navigate back after editing user info if this is the first page in history

--- src/components/editUserInfoPage.jsx
+++ src/components/editUserInfoPage.jsx
@@ -4,23 +4,30 @@ const ReactRedux = require('react-redux');
 const UserForm = require('./userForm.jsx');
 
 const navigationActions = require('../redux/actions/navigationActions');
+const navigationConstants = require('../utils/navigationConstants');
 
 function mapStateToProps(state) {
     const uidToEdit = state.routing.current.params.uid || state.authData.uid;
     return {
-        uid: uidToEdit
+        uid: uidToEdit,
+        goHomeAfterEditing: state.routing.history.length === 1
     };
 }
 
 function mapDispatchToProps(dispatch) {
     return {
-        navigateBack: pageId => dispatch(navigationActions.navigateBack())
+        navigateHome: () => dispatch(navigationActions.replace(navigationConstants.PAGES.HOME.val)),
+        navigateBack: () => dispatch(navigationActions.navigateBack())
     };
 }
 
 class EditorUserInfoPage extends React.Component {
     onFormClose = () => {
-        this.props.navigateBack();
+        if (this.props.goHomeAfterEditing) {
+            this.props.navigateHome();
+        } else {
+            this.props.navigateBack();
+        }
     };
 
     render() {
@@ -32,7 +39,9 @@ class EditorUserInfoPage extends React.Component {
 
 EditorUserInfoPage.propTypes = {
     uid: React.PropTypes.string.isRequired,
-    navigateBack: React.PropTypes.func.isRequired
+    navigateBack: React.PropTypes.func.isRequired,
+    navigateHome: React.PropTypes.func.isRequired,
+    goHomeAfterEditing: React.PropTypes.bool
 };
 
 module.exports = ReactRedux.connect(mapStateToProps, mapDispatchToProps)(EditorUserInfoPage);
