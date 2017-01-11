69768e0971c78f15373cedc1e65c1568c1ccc3b0
Amit_Kaspi <akaspi@wix.com>
2017-01-11 16:01:40 +0200

    create authStore

---
+++ mobx/stores/authStore.js
@@ -0,0 +1,21 @@
+import * as _ from 'lodash';
+import { observable, action, computed } from 'mobx';
+
+class AuthStore {
+  @observable authData = null;
+
+  @action setAuthData = authData => {
+    this.authData = authData;
+  };
+
+  @computed get isLoggedIn() {
+    return !!(_.get(this.authData, 'uid'));
+  }
+
+  @computed get isAdmin() {
+    return !!(_.get(this.authData, 'isAdmin'))
+  }
+
+}
+
+export default AuthStore;
---
+++ spec/mobx/stores/authStore.spec.js
@@ -0,0 +1,69 @@
+import AuthStore from '../../../mobx/stores/authStore';
+
+describe('AuthStore', () => {
+
+  describe('initialization', function() {
+
+    it('should be logged out', function() {
+      const authStore = new AuthStore();
+
+      expect(authStore.isLoggedIn).toBe(false);
+    });
+
+    it('should not be admin', function() {
+      const authStore = new AuthStore();
+
+      expect(authStore.isAdmin).toBe(false);
+    });
+
+  });
+
+  describe('setting auth data', function() {
+
+    it('should be logged in', function() {
+      const authStore = new AuthStore();
+
+      authStore.setAuthData({ uid: 1234 });
+
+      expect(authStore.isLoggedIn).toBe(true);
+    });
+
+    it('should should be admin', function() {
+      const authStore = new AuthStore();
+
+      authStore.setAuthData({ isAdmin: true });
+
+      expect(authStore.isAdmin).toBe(true);
+    });
+
+    it('should not be admin', function() {
+      const authStore = new AuthStore();
+
+      authStore.setAuthData({ isAdmin: false });
+
+      expect(authStore.isAdmin).toBe(false);
+    });
+
+  });
+
+  describe('setting auth data as null', function() {
+
+    it('should be logged out', function() {
+      const authStore = new AuthStore();
+
+      authStore.setAuthData(null);
+
+      expect(authStore.isLoggedIn).toBe(false);
+    });
+
+    it('should not be admin', function() {
+      const authStore = new AuthStore();
+
+      authStore.setAuthData(null);
+
+      expect(authStore.isAdmin).toBe(false);
+    });
+
+  });
+
+});
