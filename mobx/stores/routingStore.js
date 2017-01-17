7a30d9672d6d48036da97e7a6fb4949f047d68b2
Amit_Kaspi <akaspi@wix.com>
2017-01-17 17:49:04 +0200

    create routingStore

---
+++ mobx/stores/routingStore.js
@@ -0,0 +1,34 @@
+import last from 'lodash/last';
+import { observable, action, computed } from 'mobx';
+
+class RoutingStore {
+  @observable history = [{ pageId: 'auth', params: {} }];
+
+  @computed get current() {
+      return last(this.history);
+  }
+
+  @action navigateTo = (pageId, params = {}) => {
+    this.history.push({ pageId, params });
+  };
+
+  @action navigateBack = () => {
+    if (this.history.length > 1) {
+      this.history.pop();
+    }
+  };
+
+  @action replace = (pageId, params = {}) => {
+    pageId = pageId || this.current.pageId;
+    this.history[this.history.length - 1] = { pageId, params };
+  };
+
+  @action reset = (pageId, params = {}) => {
+    this.history.replace([{ pageId, params }]);
+  };
+}
+
+const routingStore = new RoutingStore();
+
+export default routingStore;
+export { RoutingStore };
---
+++ spec/mobx/stores/routingStore.spec.js
@@ -0,0 +1,72 @@
+import { RoutingStore } from '../../../mobx/stores/routingStore';
+
+describe('RoutingStore', () => {
+
+  it('should be initialized with auth pageId and empty params', () => {
+    const routingStore = new RoutingStore();
+
+    expect(routingStore.current).toEqual({ pageId: 'auth', params: {} });
+    expect(routingStore.history.length).toEqual(1);
+  });
+
+  it('should navigate to page', () => {
+    const routingStore = new RoutingStore();
+
+    routingStore.navigateTo('pageId', { p: 1 });
+
+    expect(routingStore.current).toEqual({ pageId: 'pageId', params: { p: 1 }});
+    expect(routingStore.history.length).toEqual(2);
+  });
+
+  it('should navigate back', () => {
+    const routingStore = new RoutingStore();
+
+    routingStore.navigateTo('pageId', { p: 1 });
+    routingStore.navigateTo('otherPageId', { p: 3 });
+    routingStore.navigateBack();
+
+    expect(routingStore.current).toEqual({ pageId: 'pageId', params: { p: 1 } });
+    expect(routingStore.history.length).toEqual(2);
+  });
+
+  it('should not navigate back if there is a single navigation history', () => {
+    const routingStore = new RoutingStore();
+
+    routingStore.navigateBack();
+
+    expect(routingStore.current).toEqual({ pageId: 'auth', params: {} });
+    expect(routingStore.history.length).toEqual(1);
+  });
+
+  it('should replace page', () => {
+    const routingStore = new RoutingStore();
+
+    routingStore.navigateTo('pageId', { p: 1 });
+    routingStore.replace('someOtherPage');
+
+    expect(routingStore.current).toEqual({ pageId: 'someOtherPage', params: {} });
+    expect(routingStore.history.length).toEqual(2);
+  });
+
+  it('should replace params', () => {
+    const routingStore = new RoutingStore();
+
+    routingStore.navigateTo('pageId', { p: 1 });
+    routingStore.replace(null, { p : 4 });
+
+    expect(routingStore.current).toEqual({ pageId: 'pageId', params: { p: 4 } });
+    expect(routingStore.history.length).toEqual(2);
+  });
+
+  it('should reset history', () => {
+    const routingStore = new RoutingStore();
+
+    routingStore.navigateTo('pageId', { p: 1 });
+    routingStore.navigateTo('otherPageId', { p: 3 });
+    routingStore.reset('resetPageId', { r: 8 });
+
+    expect(routingStore.current).toEqual({ pageId: 'resetPageId', params: { r: 8 } });
+    expect(routingStore.history.length).toEqual(1);
+  });
+
+});
