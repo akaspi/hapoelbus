b6fc1cd1e8e2173aa36deedca29733f0ab68e9fc
Amit_Kaspi <akaspi@wix.com>
2017-08-26 16:55:51 +0300

    teams for season 2017

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -72,6 +72,10 @@ module.exports = {
       logo: 'http://www.basket.co.il/pics/2007/logos/logo_sharon.png',
       label: 'בני הרצליה'
     },
+    'nes-ziona': {
+      logo: 'http://www.basket.co.il/Images/teams/logo_ziona.png',
+      label: 'עירוני נס ציונה'
+    },
     'kiryat-gat': {
       logo: 'http://www.basket.co.il/Images/teams/bigGat.png',
       label: 'מכבי קרית גת'
@@ -104,41 +108,25 @@ module.exports = {
       logo: 'http://www.basket.co.il/pics/2007/logos/logo_eilat.png',
       label: 'הפועל אילת'
     },
-    'ljubljana': {
-      logo: 'http://upload.wikimedia.org/wikipedia/en/thumb/8/8e/Union_olimpija.png/140px-Union_olimpija.png',
-      label: 'לובליאנה'
+    'bayern': {
+      logo: 'https://upload.wikimedia.org/wikipedia/en/f/fc/FC_Bayern_Munich_Basketball_%28emblem%29.png',
+      label: 'באיירן מינכן'
     },
-    'valencia': {
-      logo: 'http://samvanrossom.com/wp-content/uploads/2014/10/valencia-basket1-logo.jpg',
-      label: 'ולנסיה'
+    'regio-emilla': {
+      logo: 'https://upload.wikimedia.org/wikipedia/en/e/e0/Pallacanestro_Reggiana_logo.png',
+      label: 'רג׳יו אמיליה'
     },
-    'kuban': {
-      logo: 'http://upload.wikimedia.org/wikipedia/en/thumb/0/04/Lokomotiv_Kuban_logo.png/200px-Lokomotiv_Kuban_logo.png',
-      label: 'לוקומוטיב קובאן'
+    'galatasary': {
+      logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Galatasaray_Sports_Club_Logo.png/1200px-Galatasaray_Sports_Club_Logo.png',
+      label: 'גלאטסאריי'
     },
-    'fuenlabrada': {
-      logo: 'http://upload.wikimedia.org/wikipedia/en/thumb/1/13/Baloncestofuenlabrada.jpg/110px-Baloncestofuenlabrada.jpg',
-      label: 'פואנלברדה'
+    'buducnost': {
+      logo: 'https://upload.wikimedia.org/wikipedia/fr/6/6c/KKBuducnost.jpg',
+      label: 'בודנצ׳נוסט'
     },
-    'ulm': {
-      logo: 'http://nachwuchs.bbu01.com/modules/mod_bbunewsflash/assets/images/noimage_thumb.jpg',
-      label: 'אולם'
-    },
-    'vilna': {
-      logo: 'http://upload.wikimedia.org/wikipedia/en/thumb/7/75/BC_Lietuvos_Rytas_logo.svg/907px-BC_Lietuvos_Rytas_logo.svg.png',
-      label: 'ריטאס וילנה'
-    },
-    'novgorod': {
-      logo: 'http://olympiakos-live.gr/img/teams/Nizhny%20Novgorod.png',
-      label: 'ניז׳ני'
-    },
-    'zenit': {
-      logo: 'http://upload.wikimedia.org/wikipedia/commons/thumb/2/25/FC_Zenit_1_star_2015_logo.png/220px-FC_Zenit_1_star_2015_logo.png',
-      label: 'זניט'
-    },
-    'gran-canaria': {
-      logo: 'https://upload.wikimedia.org/wikipedia/en/2/2a/CB_Gran_Canaria_2014.png',
-      label: 'גראן קנריה'
+    'panevezys': {
+      logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/9/9b/BC_Lietkabelis_logo.svg/200px-BC_Lietkabelis_logo.svg.png',
+      label: 'פוניבז׳'
     }
   }
 };
