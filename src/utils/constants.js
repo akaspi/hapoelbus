9800c2bb4bcd25cdd5ef99c0bd8797d3ef222572
chene <chene@wix.com>
2018-10-06 15:34:50 +0300

    update teams for 2018-2019 season

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -21,7 +21,7 @@ module.exports = {
     }
   },
   USER: {
-    MAX_SEASON_TICKETS: 7,
+    MAX_SEASON_TICKETS: 6,
     PHONE_PREFIXES: ['050', '052', '053', '054', '055', '057', '058'],
   },
   STATIONS: {
